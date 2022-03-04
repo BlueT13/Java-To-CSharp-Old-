22d76107231136155a8314641c01b1da1b3e342f
DragonT13 <87050879+DragonT13@users.noreply.github.com>
2022-03-04 21:23:41 +0900

    ArrayLength 완성
    - 입력 값을 한번에 입력받아 스페이스로 구분하여 행렬에 저장하는 기능 구현

---
+++ Ex07/MainApp.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Ex07
+{
+	class MainApp
+	{
+		static void Main(string[] args)
+		{
+			ArrayLength arrayLength = new ArrayLength();
+			arrayLength.PrintArrayLength();
+			Console.WriteLine();
+		}
+	}
+}
